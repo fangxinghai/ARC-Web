9cc5e3a4397f16ee792f1c23eb79ee54cf5dcf43
fangxinghai <103813192+fangxinghai@users.noreply.github.com>
2026-04-03 14:39:23 +0800

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES ? "/ARC-Web/" : "/",
+  base: process.env.GITHUB_PAGES ? "/ARC-keyboard/" : "/",
   // prevent vite from obscuring rust errors
   clearScreen: false,
   // Tauri expects a fixed port, fail if that port is not available
