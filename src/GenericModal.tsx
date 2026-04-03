388d453132e604676b2fd99447d3f980228501be
fangxinghai <103813192+fangxinghai@users.noreply.github.com>
2026-04-03 12:41:53 +0800

    修复GenericModal类型

--- src/GenericModal.tsx
+++ src/GenericModal.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
 export interface GenericModalProps {
+  open?: boolean;
   onClose?: () => void;
   className?: string;
   children: React.ReactNode;
 }
 
-export const GenericModal = React.forwardRef(({ onClose, children, className }: GenericModalProps, ref: React.Ref<HTMLDialogElement>) => {
+export const GenericModal = React.forwardRef(({ open, onClose, children, className }: GenericModalProps, ref: React.Ref<HTMLDialogElement>) => {
   return (
     <dialog
       ref={ref}
+      open={open}
       onClose={onClose}
       className={`p-5 rounded-lg bg-base-100 text-base-content backdrop:bg-[rgba(0,0,0,0.5)] ${className}`}
     >
